c5857d6a0c6056887d6e4e0728891ad564052d14
Andrew Welch <andrew@nystudio107.com>
2022-03-14 16:19:49 -0400

    docs: Update to latest VitePress config

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -1,24 +1,26 @@
 module.exports = {
-    title: 'Route Map Plugin Documentation',
-    description: 'Documentation for the Route Map plugin',
-    base: '/docs/route-map/',
-    lang: 'en-US',
-    head: [
-        ['meta', { content: 'https://github.com/nystudio107', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.youtube.com/channel/UCOZTZHQdC-unTERO7LRS6FA', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also', }],
-    ],
-    themeConfig: {
-        repo: 'nystudio107/craft-routemap',
-        docsDir: 'docs/docs',
-        docsBranch: 'v1',
-        algolia: {
-            apiKey: '73ffb565bfdadaaf600202a67c17dc88',
-            indexName: 'route-map'
-        },
-        editLinks: true,
-        editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Updated',
-        sidebar: 'auto',
+  title: 'Route Map Plugin Documentation',
+  description: 'Documentation for the Route Map plugin',
+  base: '/docs/route-map/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://twitter.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://youtube.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    repo: 'nystudio107/craft-routemap',
+    docsDir: 'docs/docs',
+    docsBranch: 'v1',
+    algolia: {
+      appId: '',
+      apiKey: '',
+      indexName: ''
     },
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    sidebar: 'auto',
+  },
 };
